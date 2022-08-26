b9321f703ccd377dabd45dc7b67df5112273da42
ismail <alrrifaei51@gmail.com>
2022-08-27 02:23:05 +0300

    done !

---
+++ data.js
@@ -0,0 +1,51 @@
+let watches = [
+  {
+    id: "a",
+    img: "./images/watch1.jpeg",
+    name: "watch",
+    price: 30,
+  },
+  {
+    id: "b",
+    img: "./images/watch2.jpeg",
+    name: "watch",
+    price: 40,
+  },
+  {
+    id: "c",
+    img: "./images/watch3.jpeg",
+    name: "watch",
+    price: 25,
+  },
+  {
+    id: "d",
+    img: "./images/watch4.jpeg",
+    name: "watch",
+    price: 55,
+  },
+  {
+    id: "e",
+    img: "./images/watch5.jpeg",
+    name: "watch",
+    price: 45,
+  },
+
+  {
+    id: "f",
+    img: "./images/watch6.jpeg",
+    name: "watch",
+    price: 35,
+  },
+  {
+    id: "g",
+    img: "./images/watch7.jpeg",
+    name: "watch",
+    price: 30,
+  },
+  {
+    id: "k",
+    img: "./images/watch8.jpeg",
+    name: "watch",
+    price: 20,
+  },
+];
--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-let watches = [
-  {
-    img: "./images/watch1.jpeg",
-    name: "code:watch",
-    price: "30 k.d",
-  },
-  {
-    img: "./images/watch2.jpeg",
-    name: "code:watch",
-    price: "40 k.d",
-  },
-  {
-    img: "./images/watch3.jpeg",
-    name: "code:watch",
-    price: "50 k.d",
-  },
-];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let div = document.getElementById("Products");
+let shop = document.getElementById("shop");
 
 watches.forEach((watch) => {
-  div.innerHTML += `<div class="product-card">
+  let search = basket.find((x) => x.id === watch.id) || [];
+  shop.innerHTML += `<div id=product-id-${watch.id}  class="product-card">
     <img src="${watch.img}" id="img"/>
+    <div class="information">
     <h3 class="center">${watch.name}</h3>
-    <h4 class="center">${watch.price}</h4> 
+    <h4 class="center">${watch.price} K.D </h4> 
+    </div>
+    <div class="buttons">
+      <i onclick="decrement(${watch.id})" class="bi bi-dash-lg"></i>
+      <div id=${watch.id} class="quantity">
+      ${search.item === undefined ? 0 : search.item}
+      </div>
+      <i onclick="increment(${watch.id})" class="bi bi-plus-lg"></i>
+    </div>
     
 
-    </div>`;
+  </div>`;
 });
+
+let increment = (watch, id) => {
+  let selectedItem = watch.id;
+  let search = basket.find((x) => x.id === selectedItem);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  // console.log(basket);
+  update(selectedItem);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (watch, id) => {
+  let selectedItem = watch.id;
+  let search = basket.find((x) => x.id === selectedItem);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  // console.log(basket);
+  update(selectedItem);
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
